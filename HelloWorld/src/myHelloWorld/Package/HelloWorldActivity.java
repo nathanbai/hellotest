dfd53f91d294edd95d02407331b0a5989073ef4e
yicheng_desktop <baiyicheng@gmail.com>
2014-07-21 12:03:43 -0400

    print out first line

--- HelloWorld/src/myHelloWorld/Package/HelloWorldActivity.java
+++ HelloWorld/src/myHelloWorld/Package/HelloWorldActivity.java
@@ -1,5 +1,7 @@
 package myHelloWorld.Package;
 
+
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,5 +11,7 @@ public class HelloWorldActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        System.out.println("Test for github operation.");
     }
 }
